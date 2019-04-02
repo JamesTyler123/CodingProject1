385287ade82e13a5f2870d5af221fedb408a6422
James Tyler <jamestyler2280@gmail.com>
2019-04-02 23:37:23 +0000

    Did some various things, they dont work

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(){
+    char *hidden = "uvqqra zrffntr"; //text reads hidden message
+    int counter;
+    int i;
+    int letternum = 0;
+    for(counter = 1; counter < 26; counter++){
+        if (hidden[letternum] > 122){
+            hidden[letternum];
+        }
+        
+        while (hidden[letternum] != 0){
+            printf("%c", hidden[letternum]);
+            letternum++;
+        }
+        
+        for(i = 0; i < 20; i++){
+            hidden[i]++; 
+        }
+
+    }
 }
