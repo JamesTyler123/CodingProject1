aa76ea091e8569d2c5f050309181a4bcd5d86b92
James Tyler <jamestyler2280@gmail.com>
2019-04-03 00:42:00 +0000

    Added comments

--- main.c
+++ main.c
@@ -2,29 +2,28 @@
 #include <string.h>
 
 int main(){
-    char hidden[] = "nofbyhgr"; //this text is "hidden"
+    char hidden[] = "shfvba"; //this text is "fusion" it has two outputs
     char final[100];
     int counter;
     int i = 0;
     
-    for(counter = 1; counter < 26; counter++){
-        for(i = 0; hidden[i] != '\0'; i++){
-            if(hidden[i] != 32){
-                hidden[i]+= 1;
+    for(counter = 1; counter < 26; counter++){ //a counter for how many rotations have occured
+        for(i = 0; hidden[i] != '\0'; i++){ //a for loop stopping upon the end of string
+            if(hidden[i] != 32){ //keeps the space in characters if two words
+                hidden[i]+= 1; //increases the current letter to next, i.e. moves a to b
             }
             
-            if (hidden[i] > 'z'){
+            if (hidden[i] > 'z'){ //if character is above z, moves it back to a
                 hidden[i] = 'a';
             }
         }
     
         FILE *input;
-        input = fopen("text.txt", "r");
-        for(i = 0; i < 10000; i++){
+        input = fopen("text.txt", "r"); //opens the text file
+        for(i = 0; i < 10000; i++){ //tests all words in the file
             fscanf(input, "%s", final);
-            if(strcmp(hidden, final) == 0){
+            if(strcmp(hidden, final) == 0){ //if the string is in the file and matches the cipher, print it
                 printf("%s\n", hidden);
-                break;
             }
         }
     }
