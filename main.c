72bd9881f1f580d0dbb813838cade9d86b1d59b5
James Tyler <jamestyler2280@gmail.com>
2019-04-03 00:34:46 +0000

    Cleaned up slightly

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 int main(){
-    char hidden[] = "uvqqra"; //this text is "hidden"
+    char hidden[] = "nofbyhgr"; //this text is "hidden"
     char final[100];
     int counter;
     int i = 0;
+    
     for(counter = 1; counter < 26; counter++){
         for(i = 0; hidden[i] != '\0'; i++){
             if(hidden[i] != 32){
